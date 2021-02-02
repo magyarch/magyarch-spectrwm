687b673673624a5153ec48914724dc9935e83228
bazeeel <deakfefe77@gmail.com>
2021-02-02 19:45:28 +0100

    fix

--- .config/spectrwm/baraction.sh
+++ .config/spectrwm/baraction.sh
@@ -22,7 +22,7 @@ vol(){
 cpu(){
   read cpu a b c previdle rest < /proc/stat
   prevtotal=$((a+b+c+previdle))
-  sleep 0.5
+  sleep 0.05
   read cpu a b c idle rest < /proc/stat
   total=$((a+b+c+idle))
   cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
