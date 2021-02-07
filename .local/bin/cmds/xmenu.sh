59f8e40e67634ca42591d60d51ebc5e1b6363540
bazeeel <deakfefe77@gmail.com>
2021-02-07 21:54:22 +0100

    update wal

--- .local/bin/cmds/xmenu.sh
+++ .local/bin/cmds/xmenu.sh
@@ -5,7 +5,7 @@ cat <<EOF | xmenu | sh &
 	 Brave		brave
 	 Terminal	alacritty
 	 Files		pcmanfm
-	 Sublime Text	subl3		
+	 Sublime Text	subl3
 	 Steam		steam
 	 Video		mpv
 	 Music		urxvt -e ncmpcpp
@@ -13,7 +13,7 @@ cat <<EOF | xmenu | sh &
 	 Network	urxvt -e nmtui
 
  Screenshot
-	 Full		maim ~/Képek/Képernyőképek/`date +%Y-%m-%d_%H:%M:%S`.png 
+	 Full		maim ~/Képek/Képernyőképek/`date +%Y-%m-%d_%H:%M:%S`.png
 	 Pick		maim -s ~/Képek/Képernyőképek/`date +%y%m%d-%H%M-%S`.png
 
  System
