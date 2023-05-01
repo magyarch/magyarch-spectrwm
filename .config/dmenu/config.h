6b30b7b5e65205552b24e0fb7d20ecc6d01ed364
bazeeel <deakfefe77@gmail.com>
2023-05-01 12:17:38 +0200

    upload

--- .config/dmenu/config.h
+++ .config/dmenu/config.h
@@ -2,29 +2,26 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 600;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono Nerd Font:size=12"
+	"MesloLGS Nerd Font:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#c3cdc8", "#947cc3" },
-	[SchemeSel] = { "#c3cdc8", "#282a36" },
-	[SchemeSelHighlight] = { "#c3cdc8", "#947cc3" },
-	[SchemeNormHighlight] = { "#c3cdc8", "#282a36" },
-	[SchemeOut] = { "#c3cdc8", "#947cc3" },
-   /* [SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#328a6a", "#222222" },
-	[SchemeNormHighlight] = { "#328a6a", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },*/
+        [SchemeNorm] = { "#bbbbbb", "#282a36" },
+	[SchemeSel] = { "#b5bd68", "#947cc3" },
+    [SchemeSelHighlight] = { "#947cc3", "#282a36" },
+    [SchemeNormHighlight] = { "#947cc3", "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+
+
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 0;
-static unsigned int columns    = 0;
+static unsigned int lines      = 20;
+static unsigned int columns    = 1;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
 static unsigned int min_lineheight = 8;
@@ -36,4 +33,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 8;
